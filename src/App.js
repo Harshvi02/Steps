95066ff74af7b95266665882972a7cd929981f74
Harshvi02 <230493131002.harshvi@gmail.com>
2026-02-20 21:20:14 +0530

    Implement Steps project using React with useState hook, step navigation functionality, dynamic message display, step indicators with conditional styling, and close toggle feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <Steps />
     </div>
   );
 }
 
-export default App;
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+  const messages = [
+    "Learn React ⚛️",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+
+  // Previous function
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  // Next function
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
+
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+  ×
+   </button>
+       <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+
+
+  
+      <p>
+        Step {step}: {messages[step - 1]}
+      </p>
+     
+      <button onClick={handlePrevious}>
+        Previous
+      </button>
+
+      <button onClick={handleNext}>
+        Next
+      </button>
+    </div>
+  );
+}
